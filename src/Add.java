a5b5a0e7edbc7a62a46491dc4aa158575e243eb5
ANAS011192007 <achowdhury192007@bscse.uiu.ac.bd>
2022-05-11 19:36:19 +0600

    variable name of n1 changed to n0 in add.java

--- src/Add.java
+++ src/Add.java
@@ -1,8 +1,8 @@
 public class Add {
     public static void main(String args[])
     {
-        int n1 = 225, n2 = 115, n3 = 120, sum;
-        sum = n1 + n2 + n3;
+        int n0 = 225, n2 = 115, n3 = 120, sum;
+        sum = n0 + n2 + n3;
         System.out.println("The sum of numbers is: "+sum);
     }
 }
